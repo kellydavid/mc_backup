fd7828b680b44822d05362263c6d10af778905fc
kellydavid <kellydavid178@gmail.com>
2014-07-20 11:45:23 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Minecraft Backup Script
+=======================
+
+mc_backup.sh is a simple bash script that will create a tar archive of a minecraft server and then upload the server to dropbox using dropbox_uploader.sh
---
+++ mc_backup.sh
@@ -0,0 +1,14 @@
+#First parameter is the name of the server
+#Second parameter is the directory that the server runs in.
+#Third parameter is the directory that the server will be backed up to.
+# PATHS TO DIRECTORIES MUST BE ABSOLUTE PATH NAMES
+SERVER_NAME=$1
+SERVER_DIRECTORY=$2
+BACKUP_DIRECTORY=$3
+CURRENT_DATE=$(date +"%d-%m-%Y") 
+BACKUP_FILE=$BACKUP_DIRECTORY"/"$SERVER_NAME"_"$CURRENT_DATE
+
+# Create backup
+echo Backing up $SERVER_NAME at $SERVER_DIRECTORY
+tar -cpvzf $BACKUP_FILE.tar.gz $SERVER_DIRECTORY
+echo Backup created at $BACKUP_FILE.tar.gz
