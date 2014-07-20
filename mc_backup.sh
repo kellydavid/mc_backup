7199bf0111d3e26354aa0683f10db563a5295982
kellydavid <kellydavid178@gmail.com>
2014-07-20 12:07:55 -0400

    Added ability to upload backup file to dropbox app using the dropbox uploader.

--- mc_backup.sh
+++ mc_backup.sh
@@ -6,9 +6,10 @@ SERVER_NAME=$1
 SERVER_DIRECTORY=$2
 BACKUP_DIRECTORY=$3
 CURRENT_DATE=$(date +"%d-%m-%Y") 
-BACKUP_FILE=$BACKUP_DIRECTORY"/"$SERVER_NAME"_"$CURRENT_DATE
+BACKUP_FILE=$BACKUP_DIRECTORY"/"$SERVER_NAME"_"$CURRENT_DATE.tar.gz
 
 # Create backup
 echo Backing up $SERVER_NAME at $SERVER_DIRECTORY
-tar -cpvzf $BACKUP_FILE.tar.gz $SERVER_DIRECTORY
-echo Backup created at $BACKUP_FILE.tar.gz
+tar -cpvzf $BACKUP_FILE $SERVER_DIRECTORY
+echo Backup created at $BACKUP_FILE
+/bin/bash ~/scripts/Dropbox-Uploader/dropbox_uploader.sh upload $BACKUP_FILE $SERVER_NAME
